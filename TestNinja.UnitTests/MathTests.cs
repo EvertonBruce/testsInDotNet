0c3d129845f61f8dc02923f3f1f1791df7fc9d20
Everton Bruce <evertonflbruce@gmail.com>
2025-02-27 21:48:28 -0300

    Implementing new tests and using NUnit functionalities

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+
+    public class MathTests
+    {
+        private Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
+        [Test]
+        public void Add_WhenCalled_ReturnSumOfArguments()
+        {
+            var result = _math.Add(1, 3);
+
+            Assert.That(result, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+        {
+            var result = _math.Max(2, 1);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
+        {
+            var result = _math.Max(1, 2);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+        
+        [Test]
+        public void Max_BothArgumentsAreEqual_ReturnTheSame()
+        {
+            var result = _math.Max(2, 2);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+    }
+}
