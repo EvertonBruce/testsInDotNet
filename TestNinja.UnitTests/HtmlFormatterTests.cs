2bf06ef95900b0c604d4cd638b2b80529b5ea06a
Everton Bruce <evertonflbruce@gmail.com>
2025-03-24 22:02:37 -0300

    Implementing string tests and improving therepeating code on mathTest.

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+	[TestFixture]
+	public class HtmlFormatterTests
+	{
+		[Test]
+		public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithTheStrongElement()
+		{
+			var formater = new HtmlFormatter();
+
+			var result = formater.FormatAsBold("Test");
+
+			// Specific
+			Assert.That(result, Is.EqualTo("<strong>Test</strong>"));
+
+			// More general
+			Assert.That(result, Does.StartWith("<strong>"));
+			Assert.That(result, Does.EndWith("</strong>"));
+			Assert.That(result, Does.Contain("Test"));
+		}
+	}
+}
