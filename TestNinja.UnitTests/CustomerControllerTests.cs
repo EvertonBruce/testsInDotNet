293ac7bf89226b5fa9e6c5a8e6dfc8fbde30f0ad
Everton Bruce <evertonflbruce@gmail.com>
2025-03-25 22:05:33 -0300

    Addint tests of arrays and return types.

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+	[TestFixture]
+	public class CustomerControllerTests
+	{
+		private CustomerController _customer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _customer = new CustomerController();
+        }
+
+        [Test]
+		public void GetCustomer_IdIsZero_ReturnNotFound()
+		{
+			var result = _customer.GetCustomer(0);
+
+			//NotFound
+			Assert.That(result, Is.TypeOf<ActionResult>());
+
+			//NotFound or one of its derivatives 
+			Assert.That(result, Is.InstanceOf<ActionResult>());
+		}
+	}
+}
--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -37,5 +37,45 @@ namespace TestNinja.UnitTests
 
             Assert.That(result, Is.EqualTo(expectedResult));
         }
+
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnOddNumbersUpToLimit()
+        {
+            var result = _math.GetOddNumbers(5);
+
+            ////Generical aproach
+            //Assert.That(result, Is.Not.Empty);
+
+            ////Less General Aproach
+            //Assert.That(result.Count(), Is.EqualTo(3));
+
+            ////Especific aproach
+            //Assert.That(result, Does.Contain(1));
+            //Assert.That(result, Does.Contain(3));
+            //Assert.That(result, Does.Contain(5));
+
+            //Compact Especific aproach
+            Assert.That(result, Is.EquivalentTo(new[] { 1, 3, 5 }));
+
+            //Usefull assertions
+            //Assert.That(result, Is.Ordered); //Garantee that your collection is ordered
+            //Assert.That(result, Is.Unique); // Gatantee that your collection has no duplicates in it
+        }
+
+        [Test]
+        public void GetOddNumbers_LimitIsEqualToZero_ReturnAnEmptyArray()
+        {
+            var result = _math.GetOddNumbers(0);
+
+            Assert.That(result, Is.Empty);
+        }
+        
+        [Test]
+        public void GetOddNumbers_LimitIsMinorThenZero_ReturnAnEmptyArray()
+        {
+            var result = _math.GetOddNumbers(-3);
+
+            Assert.That(result, Is.Empty);
+        }
     }
 }
