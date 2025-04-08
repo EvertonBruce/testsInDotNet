a26e40f91b6d378afc6f0a3f7d093d4bb48d2242
Everton Bruce <evertonflbruce@gmail.com>
2025-04-07 21:22:33 -0300

    Implementing dependency injection to implement tests.

---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,21 @@
+﻿
+
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+	[TestFixture]
+	public class VideoServiceTests
+	{
+		[Test]
+		public void ReadVideoTitle_EmptyFile_ReturnsError()
+		{
+			var service = new VideoService(new MockFileReader());
+
+			var result = service.ReadVideoTitle();
+
+			Assert.That(result, Does.Contain("error").IgnoreCase);
+		}
+	}
+}
