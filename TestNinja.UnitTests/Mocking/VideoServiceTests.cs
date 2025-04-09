057c41826b33ed71f7876ef41e106e2df36a0bd3
Everton Bruce <evertonflbruce@gmail.com>
2025-04-08 21:55:35 -0300

    Add mock using moq package.

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,5 +1,4 @@
-﻿
-
+﻿using Moq;
 using NUnit.Framework;
 using TestNinja.Mocking;
 
@@ -8,12 +7,22 @@ namespace TestNinja.UnitTests.Mocking
 	[TestFixture]
 	public class VideoServiceTests
 	{
+		private VideoService _videoService;
+		private Mock<IFileReader> _fileReader;
+
+		[SetUp]
+        public void SetUp()
+		{
+			_fileReader = new Mock<IFileReader>();
+			_videoService = new VideoService(_fileReader.Object);
+		}
+
 		[Test]
 		public void ReadVideoTitle_EmptyFile_ReturnsError()
 		{
-			var service = new VideoService(new MockFileReader());
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
 
-			var result = service.ReadVideoTitle();
+			var result = _videoService.ReadVideoTitle();
 
 			Assert.That(result, Does.Contain("error").IgnoreCase);
 		}
