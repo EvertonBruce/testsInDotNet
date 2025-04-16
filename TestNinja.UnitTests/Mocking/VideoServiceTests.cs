321526699a941f44cdb8a86d8ecd1daee604abba
Everton Bruce <evertonflbruce@gmail.com>
2025-04-15 21:56:27 -0300

    Implementing tests for other functions inside VideoService.

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,5 +1,7 @@
 ﻿using Moq;
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.Data.Entity;
 using TestNinja.Mocking;
 
 namespace TestNinja.UnitTests.Mocking
@@ -8,13 +10,15 @@ namespace TestNinja.UnitTests.Mocking
 	public class VideoServiceTests
 	{
 		private VideoService _videoService;
+		private Mock<IVideoRepository> _videoRepository;
 		private Mock<IFileReader> _fileReader;
 
 		[SetUp]
         public void SetUp()
 		{
 			_fileReader = new Mock<IFileReader>();
-			_videoService = new VideoService(_fileReader.Object);
+			_videoRepository = new Mock<IVideoRepository>();
+            _videoService = new VideoService(_fileReader.Object, _videoRepository.Object);
 		}
 
 		[Test]
@@ -26,5 +30,31 @@ namespace TestNinja.UnitTests.Mocking
 
 			Assert.That(result, Does.Contain("error").IgnoreCase);
 		}
-	}
+
+		[Test]
+		public void GetUnprocessedVideosAsCsv_AreThereUnprocessedVideos_ReturnsTheRightIds()
+		{
+			_videoRepository.Setup(vr => vr.GetUnprocessedVideos()).Returns(new List<Video>()
+			{
+				new Video{Id = 1},
+				new Video{Id = 2},
+				new Video{Id = 3},
+				new Video{Id = 4}
+			});
+
+			var result = _videoService.GetUnprocessedVideosAsCsv();
+
+			Assert.That(result, Is.EqualTo("1,2,3,4"));
+		}
+
+        [Test]
+        public void GetUnprocessedVideosAsCsv_AllVideosAreProcessed_ReturnsEmptyString()
+        {
+			_videoRepository.Setup(vr => vr.GetUnprocessedVideos()).Returns(new List<Video>());
+
+            var result = _videoService.GetUnprocessedVideosAsCsv();
+
+            Assert.That(result, Is.EqualTo(""));
+        }
+    }
 }
